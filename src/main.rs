30e21f508a7a28eba3f11e251d69a57eb3009981
fuluz <fuluz@fuluzs-MacBook-Pro.local>
2023-10-07 15:01:22 +0700

    update struct query

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,17 @@
 use reqwest::Error;
 use serde::{Deserialize, Serialize};
+mod query_struct;
 
-#[derive(Debug, Serialize, Deserialize)]
-struct Query {
-    chain: String,
-    tokenIn: String,
-    tokenOut: String,
-    amountIn: String,
-}
+use query_struct::{Query, SwapResponse};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let client = reqwest::Client::new();
-    let query = Query {
+    let _query = Query {
         chain: "arbitrum".into(),
-        tokenIn: "0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1".into(),
-        tokenOut: "0x894134a25a5faC1c2C26F1d8fBf05111a3CB9487".into(),
-        amountIn: "3000000000000000000000".into(),
+        token_in: "0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1".into(),
+        token_out: "0x894134a25a5faC1c2C26F1d8fBf05111a3CB9487".into(),
+        amount_in: "3000000000000000000000".into(),
     };
     let res = client
         .get("https://aggregator-api.kyberswap.com/arbitrum/api/v1/routes?tokenIn=0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1&tokenOut=0xd85E038593d7A098614721EaE955EC2022B9B91B&amountIn=5000000000000000000000&saveGas=false&gasInclude=true")
@@ -24,6 +19,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .header("User-Agent", "Mozilla")
         .send()
         .await?;
-    println!("{:#?}", res.text().await?);
+
+    let result: SwapResponse = res.json().await?;
+    println!("{:#?}", result.data.route_summary.amount_out);
     Ok(())
 }
---
+++ src/query_struct.rs
@@ -0,0 +1,87 @@
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+#[derive(Serialize, Deserialize)]
+pub struct Query {
+    pub chain: String,
+    pub token_in: String,
+    pub token_out: String,
+    pub amount_in: String,
+}
+#[derive(Debug, Serialize, Deserialize)]
+pub struct SwapResponse {
+    pub code: i64,
+    pub message: String,
+    pub data: Data,
+    //pub request_id: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Data {
+    pub route_summary: RouteSummary,
+    router_address: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct RouteSummary {
+    token_in: String,
+    amount_in: String,
+    amount_in_usd: String,
+    token_in_market_price_available: bool,
+    token_out: String,
+    pub amount_out: String,
+    amount_out_usd: String,
+    token_out_market_price_available: bool,
+    gas: String,
+    gas_price: String,
+    gas_usd: String,
+    extra_fee: ExtraFee,
+    route: Vec<Vec<Route>>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ExtraFee {
+    fee_amount: String,
+    charge_fee_by: String,
+    is_in_bps: bool,
+    fee_receiver: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Route {
+    pool: String,
+    token_in: String,
+    token_out: String,
+    limit_return_amount: String,
+    swap_amount: String,
+    amount_out: String,
+    exchange: String,
+    pool_length: i64,
+    pool_type: String,
+    pool_extra: Option<PoolExtra>,
+    extra: Option<Extra>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Extra {}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct PoolExtra {
+    vault: Option<String>,
+    pool_id: Option<String>,
+    map_token_address_to_index: Option<HashMap<String, i64>>,
+    stable: Option<bool>,
+    swap_fee: Option<SwapFee>,
+    fee_precision: Option<i64>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(untagged)]
+pub enum SwapFee {
+    Integer(i64),
+    String(String),
+}
